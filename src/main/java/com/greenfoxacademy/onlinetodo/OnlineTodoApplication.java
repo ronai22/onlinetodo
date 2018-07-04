7006aa47b0edc7206cc56000f47112f098d90989
ronai22 <ronai22@gmail.com>
2018-07-04 16:17:02 +0200

    init(todoapp): inititalize project

---
+++ src/main/java/com/greenfoxacademy/onlinetodo/OnlineTodoApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.onlinetodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineTodoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OnlineTodoApplication.class, args);
+  }
+}
